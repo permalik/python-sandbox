d44fb1c9a4d77fa75fc2df1caedb8ce123d135f0
permalik <permalik@protonmail.com>
2025-03-27 00:20:40 -0500

    feat: impl devshell

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "python_curricula";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = false;
+        };
+        myPython = pkgs.python312;
+        pythonWithPkgs = myPython.withPackages (pythonPkgs:
+          with pythonPkgs; [
+            pip
+            virtualenvwrapper
+          ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.alejandra
+            pythonWithPkgs
+            pkgs.readline
+          ];
+
+          shellHook = ''
+            VENV=.venv
+            if [ ! -d $VENV ]; then
+                virtualenv $VENV
+            fi
+            source ./$VENV/bin/activate
+            echo "Python virtual environment activated."
+
+            # Custom Prompt
+            export PS1="\n\[\e[1;32m\][devshell](.venv) \w\n❯ \[\e[0m\]"
+          '';
+        };
+      }
+    );
+}
