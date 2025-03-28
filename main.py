6d205e0785386344578d24af6a21b6558fbc16a3
permalik <permalik@protonmail.com>
2025-03-27 20:27:18 -0500

    feat: outline cli args

--- main.py
+++ main.py
@@ -14,38 +14,35 @@ def main():
         dest="category", required=True, help="Select a Category"
     )
 
-    """
-    Fundamental Category
-    """
+    # Category Selection
+    # Fundamental
     fun_parser = subparsers.add_parser("fun", help="Fundamental Programs")
     fun_subparsers = fun_parser.add_subparsers(dest="program", required=True)
 
-    """
-    Fundamental Category
-    """
+    # Construct
+    con_parser = subparsers.add_parser("con", help="Construct Programs")
+    con_subparsers = con_parser.add_subparsers(dest="construct", required=True)
+
+    # Datastructure
+    ds_parser = subparsers.add_parser("ds", help="Datastructure Programs")
+    ds_subparsers = ds_parser.add_subparsers(dest="ds", required=True)
+
+    # Algorithm
+    alg_parser = subparsers.add_parser("alg", help="Algorithm Programs")
+    alg_subparsers = alg_parser.add_subparsers(dest="alg", required=True)
+
+    # Calc Subcategory
+    # Fibonacci V1
     fib_v1_parser = fun_subparsers.add_parser(
-        "fibonacci_v1", help="Computer Fibonacci Sequence"
+        "fibonacci_v1", help="Compute Fibonacci Sequence"
     )
     fib_v1_parser.add_argument(
         "-n", "--iterations", type=int, default=10, help="Number of iterations"
     )
     fib_v1_parser.set_defaults(func=v1_fibonacci)
 
-    # parser.add_argument("con", help="Category: construct")
-    # parser.add_argument("ds", help="Category: datastructure")
-    # parser.add_argument("alg", help="Category: algorithm")
-    #
-    # """
-    # Program Selection
-    # """
-    # parser.add_argument("program", help="Program to run")
-
     args = parser.parse_args()
     args.func(args)
-
-    if args.program == "v1_fibonacci":
-        for i in range(10):
-            print(v1_fibonacci(i), " ", end="")
 
 
 if __name__ == "__main__":
